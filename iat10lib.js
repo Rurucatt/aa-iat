9b0d11784e6bab4a0a0313948c5f9c391eabce77
Rurucatt <189954343+Rurucatt@users.noreply.github.com>
2026-03-01 02:15:21 -0500

    Update iat10lib.js

--- iat10lib.js
+++ iat10lib.js
@@ -116,13 +116,13 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			
 			////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
-			blockAttributes_nTrials : 20,
+			blockAttributes_nTrials : 2,
 			blockAttributes_nMiniBlocks : 5,
-			blockCategories_nTrials : 20,
+			blockCategories_nTrials : 2,
 			blockCategories_nMiniBlocks : 5,
-			blockFirstCombined_nTrials : 20,
+			blockFirstCombined_nTrials : 2,
 			blockFirstCombined_nMiniBlocks : 5,
-			blockSecondCombined_nTrials : 40, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
+			blockSecondCombined_nTrials : 4, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 10, 
 			blockSwitch_nTrials : 28,
 			blockSwitch_nMiniBlocks : 7,
